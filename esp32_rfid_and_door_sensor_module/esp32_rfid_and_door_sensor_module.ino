dee4af4fd1b943b3ed2bafafc81596135485666c
Eivind Gussiås Løkseth <eivind@outlook.com>
2025-07-30 23:32:51 +0200

    Issue #1 - Use file with valid RFID values and validate cards before opening door

--- esp32_rfid_and_door_sensor_module/esp32_rfid_and_door_sensor_module.ino
+++ esp32_rfid_and_door_sensor_module/esp32_rfid_and_door_sensor_module.ino
@@ -10,9 +10,18 @@
 //#include <MFRC522DriverI2C.h>
 #include <MFRC522DriverPinSimple.h>
 #include <MFRC522Debug.h>
+
+// Adapted from: https://github.com/espressif/arduino-esp32/blob/master/libraries/LittleFS/examples/LITTLEFS_test/LITTLEFS_test.ino
+// Project details: https://RandomNerdTutorials.com/esp32-write-data-littlefs-arduino/
+#include <Arduino.h>
+#include "FS.h"
+#include <LittleFS.h>
+#define FORMAT_LITTLEFS_IF_FAILED true
+
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <atomic>
+#include <vector>
 #include "time.h"
 #include "esp_sntp.h"
 #include "parameters.h"
@@ -26,6 +35,7 @@ const int MagnetSensorPin = 15;
 char doorSensorState[7] = "closed";
 std::atomic<bool> doorSensorStateChanged = false;
 unsigned long doorSensorChangedTime = 0;
+std::vector<String> validRfidValues = { };
 
 // Learn more about using SPI/I2C or check the pin assigment for your board: https://github.com/OSSLibraries/Arduino_MFRC522v2#pin-layout
 MFRC522DriverPinSimple ss_pin(5);
@@ -151,6 +161,60 @@ void subscribeToMqttTopics() {
   }
 }
 
+bool ValidateRfid(String rfid) {
+  if (rfid.length() == 0) {
+    return false;
+  }
+
+  Serial.printf("Validating %s. ", rfid);
+  for (String validRfid : validRfidValues) {
+    Serial.printf("Comparing it to %s. ", validRfid);
+    if (rfid == validRfid) {
+      Serial.printf("%s is valid.\n", rfid);
+      return true;
+    }
+  }
+
+  Serial.printf("%s is NOT valid.\n", rfid);
+  return false;
+}
+
+void UpdateFileOfValidRfidValues(String mqttMessage) {
+  File file = LittleFS.open("/rfid-values.txt", "w");
+  file.print(mqttMessage);
+  file.close();
+  Serial.println("Updated /rfid-values.txt");
+}
+
+void ReadFileOfValidRfidValues() {
+  File file = LittleFS.open("/rfid-values.txt", "r");
+
+  if (!file) {
+    Serial.println("Failed to open rfid-values.txt for reading");
+    return;
+  }
+
+  validRfidValues.clear();
+  String rfid = "";
+  Serial.println("File Content:");
+  while (file.available()) {
+    // Read each byte and append to the rfid string until line shift char(10), 
+    // then store all RFID values in memory as a vector until all file content is read
+    char c = file.read();
+
+    if (c == '\n') {
+      // We've read an RFID value, add it to the vector and clear rfid string to read the next one
+      validRfidValues.push_back(rfid);
+      Serial.printf(" Read an RFID: %s\n", rfid);
+      rfid = "";
+    } else {
+      rfid += c;
+      Serial.write(c);
+    }    
+  }
+  file.close();
+}
+
 void FeedbackForGrantedAccess() {
   digitalWrite(BlueLedPin, LOW);
   digitalWrite(GreenLedPin, HIGH);
@@ -220,6 +284,29 @@ void setup() {
   mfrc522.PCD_Init();    // Init MFRC522 board.
   MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial);	// Show details of PCD - MFRC522 Card Reader details.
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+
+  // Initialize LittleFS to read and store valid RFID values
+  if(!LittleFS.begin(FORMAT_LITTLEFS_IF_FAILED)){
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
+  }
+
+  // Provide some test data in /rfid-values.txt if file doesn't already exist
+  File file = LittleFS.open("/rfid-values.txt", "r");
+  if (!file) {
+    file.close();
+    Serial.println("No /rfid-values.txt file exists, so create one with dummy data.");
+    // Dummy data to test list of valid RFID values
+    String dummyMqttMessage = "b338ec13\nc30e262a\n";  // Replace these values with your own RFID Cards IDs
+    UpdateFileOfValidRfidValues(dummyMqttMessage);
+  } else {
+    file.close();
+    Serial.println("Using /rfid-values.txt to validate RFID cards");
+  }
+
+  // Read file of valid RFID cards into memory
+  ReadFileOfValidRfidValues();
+
 
   // First step is to configure WiFi STA and connect in order to get the current time and date.
   Serial.printf("Connecting to %s ", wifi_ssid);
@@ -308,8 +395,12 @@ void loop() {
   }
 
   if (uidString != "") {
-    FeedbackForGrantedAccess();
-    Serial.println(uidString);
+if (ValidateRfid(uidString)) {
+      AccessGranted();
+      Serial.printf("Opened door for RFID %s\n", uidString);
+    } else {
+      Serial.printf("WARNING: %s is NOT valid for this door\n", uidString);
+    }
   }
 
   delay(100);
